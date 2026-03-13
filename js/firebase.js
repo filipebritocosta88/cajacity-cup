1557f9de0cbc37a498f25032776769d00c96a081
filipebritocosta88 <filipebritocosta88@gmail.com>
2026-03-13 12:39:23 -0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHdQFLQ3V4BjdYRt5J6zq1H86jFJrotic",
+  authDomain: "cajacity-cup.firebaseapp.com",
+  projectId: "cajacity-cup",
+  storageBucket: "cajacity-cup.firebasestorage.app",
+  messagingSenderId: "922666284836",
+  appId: "1:922666284836:web:4bd0e3e8a6600e0dd7a5b1"
+};
+
+export const app = initializeApp(firebaseConfig);
